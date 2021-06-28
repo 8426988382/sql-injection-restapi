f0053d5ff4c5214d5bb0bc7415c2863907ce6107
tushartambi <tushartambi@gmail.com>
2021-06-28 16:01:23 +0530

    cors added

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ import sqlite3
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask_jwt import JWT, jwt_required
+from flask_cors import CORS
 
 from security import authenticate, identity
 from user import User
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = '1234addfdg'
 api = Api(app)
 
