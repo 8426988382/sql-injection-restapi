7e09a289dc09f5e20c87227314263ca8fc257b10
tushartambi <tushartambi@gmail.com>
2021-06-28 16:30:25 +0530

    cors added

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import sqlite3
 
+import flask
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask_jwt import JWT, jwt_required
+from flask_cors import CORS
 
 from security import authenticate, identity
 from user import User
 
 app = Flask(__name__)
+CORS(app)
 app.secret_key = '1234addfdg'
 api = Api(app)
 
@@ -44,7 +47,10 @@ class UserRegister(Resource):
         connection.commit()
         connection.close()
 
-        return {'message': 'user created'}, 201
+        response = flask.jsonify({'message': 'user_created'})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+
+        return response, 201
 
 
 class Card(Resource):
