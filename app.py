3733c20ef57094270f2d489ac2096c915675ac81
tushartambi <tushartambi@gmail.com>
2021-06-28 16:21:27 +0530

    cors added

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ import sqlite3
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask_jwt import JWT, jwt_required
-from flask_cors import CORS
 
 from security import authenticate, identity
 from user import User
 
 app = Flask(__name__)
-# CORS(app)
 app.secret_key = '1234addfdg'
 api = Api(app)
 
@@ -127,4 +125,4 @@ api.add_resource(UserRegister, '/user/register')
 api.add_resource(Card, '/card/<string:username>')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
