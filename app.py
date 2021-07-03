30f6e0bf418e2fab0ce7a73f8f277ace75ca457a
tushartambi <tushartambi@gmail.com>
2021-07-03 20:50:51 +0530

    Expiration Time delay added.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 from flask_cors import CORS
-from datetime
+import datetime
 
 from security import authenticate, identity
 from resources.user import UserRegister
